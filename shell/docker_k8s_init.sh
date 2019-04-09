08025c52a53f6691a238207291b1f667dba112a9
regend <kanecake@qq.com>
2019-04-09 07:03:36 +0000

    Update docker_k8s_init.sh

--- shell/docker_k8s_init.sh
+++ shell/docker_k8s_init.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 sed -i '$aproxy=http://10.10.17.8:808/' /etc/yum.conf
+yum install -y bash-completion
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --enable extras
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
@@ -40,7 +41,7 @@ gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg https://packages.cl
 EOF
 setenforce 0
 sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
-yum install -y kubelet-1.11.0 kubeadm-1.11.0 kubectl-1.11.0
+yum install -y kubelet-1.11.5 kubeadm-1.11.5 kubectl-1.11.5
 systemctl enable kubelet && systemctl start kubelet
 echo "source <(kubectl completion bash)" >> ~/.bashrc
 swapoff -a
@@ -54,6 +55,8 @@ source /usr/share/bash-completion/completions/docker
 source /etc/bash_completion.d/docker-compose
 source /usr/share/bash-completion/bash_completion
 source ~/.bashrc
+
+sed -i 's/\/dev\/mapper\/centos-swap/# \/dev\/mapper\/centos-swap/g' /etc/fstab
 
 systemctl stop firewalld.service
 systemctl disable firewalld.service
